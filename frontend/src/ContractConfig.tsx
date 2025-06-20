28a397719df82ed4492d5d27df49624f728f37ed
Filip Krawczyk <kotmawszy@gmail.com>
2025-06-21 01:33:53 +0200

    Fix chainid

--- frontend/src/ContractConfig.tsx
+++ frontend/src/ContractConfig.tsx
@@ -1,7 +1,7 @@
 import { abi } from './abi'
 
 export const contractConfig = {
-    chainId: (import.meta.env.VITE_CHAIN_ID ?? 31337) as number,
+    chainId: (parseInt(import.meta.env.VITE_CHAIN_ID) ?? 31337) as number,
     address: (import.meta.env.VITE_CONTRACT_ADDRESS ?? '0x5fbdb2315678afecb367f032d93f642f64180aa3') as `0x${string}`,
     abi,
 }
