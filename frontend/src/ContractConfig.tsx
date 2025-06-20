0237665d44ba6182af7aed8a6a80cb5e81fedae5
Filip Krawczyk <kotmawszy@gmail.com>
2025-06-21 00:58:59 +0200

    Fix .env handling of frontend

--- frontend/src/ContractConfig.tsx
+++ frontend/src/ContractConfig.tsx
@@ -1,6 +1,6 @@
 import { abi } from './abi'
 
 export const contractConfig = {
-    address: (process.env.CONTRACT_ADDRESS ?? '0x5fbdb2315678afecb367f032d93f642f64180aa3') as `0x${string}`,
+    address: (import.meta.env.CONTRACT_ADDRESS ?? '0x5fbdb2315678afecb367f032d93f642f64180aa3') as `0x${string}`,
     abi,
 }
